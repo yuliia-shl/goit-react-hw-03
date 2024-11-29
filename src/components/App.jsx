4e1b44cde3562ac7532eecbab83233acfd496923
Yuli <yu.expert@gmail.com>
2024-11-29 02:57:17 +0200

    Finish home work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,74 @@
 import 'modern-normalize';
 import '../index.css';
-import { useEffect, useState } from 'react';
 import './App.css';
-import Description from './Description/Description';
-import Options from './Options/Options';
-import Feedback from './Feedback/Feedback';
-import Notification from './Notification/Notification';
+
+import ContactList from './ContactList/ContactList';
+import ContactForm from './ContactForm/ContactForm';
+import SearchBox from './SearchBox/SearchBox';
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
 
 function App() {
   // Початковий стан
-  const initFeedback = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  // Стан для відгуків
-  const [feedback, setFeedback] = useState(() => {
-    const savedFeedback = window.localStorage.getItem('feedback');
-    if (savedFeedback !== null) {
-      return JSON.parse(savedFeedback);
-    }
-    return initFeedback;
-  });
+  const contactsData = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
 
-  // Функція для обробки відгуків
-  const updateFeedback = feedbackType => {
-    setFeedback(prev => ({
-      ...prev,
-      [feedbackType]: prev[feedbackType] + 1,
-    }));
-  };
-  // Функція для скидання відгуків
-  const handleReset = () => {
-    setFeedback(initFeedback);
-  };
+  // Під час завантаження застосунку контакти, якщо такі є, зчитуються з локального сховища і записуються у стан.
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? contactsData
+  );
 
-  const { good, neutral, bad } = feedback;
-
-  const totalFeedback = good + neutral + bad;
-  // Процент позитивних відгуків
-  const positiveFeedback = Math.round((good / totalFeedback) * 100);
-
-  // Записуємо відгуки в локальну пам'ять
+  // Застосунок зберігає масив контактів між оновленням сторінки в локальному сховищі.
   useEffect(() => {
-    window.localStorage.setItem('feedback', JSON.stringify(feedback));
-  }, [feedback]);
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  // Додати контакт
+  const handleAddContact = (name, number) => {
+    if (!name.trim() || !number.trim()) {
+      alert('Required!');
+      return;
+    }
+    const newContactData = {
+      id: nanoid(4),
+      name: name.trim(),
+      number: number.trim(),
+    };
+    setContacts(prev => [...prev, newContactData]);
+  };
+
+  // Видалити контакт
+  const handleDeleteContact = id => {
+    setContacts(prev => prev.filter(contact => contact.id !== id));
+  };
+
+  // Шукати контакт
+  const [searchValue, setSearchValue] = useState('');
+  const handleSearch = e => {
+    e.preventDefault();
+    setSearchValue(e.target.value);
+  };
+
+  // Фільтруємо контакти згідно значення в input та скидаємо регістр
+  const filterContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
 
   return (
     <>
-      <Description />
-
-      <Options
-        feedback={feedback}
-        updateFeedback={updateFeedback}
-        totalFeedback={totalFeedback}
-        reset={handleReset}
-      />
-      {totalFeedback ? (
-        <Feedback
-          feedback={feedback}
-          totalFeedback={totalFeedback}
-          positiveFeedback={positiveFeedback}
+      <div className="mainWrap">
+        <h1>Phonebook</h1>
+        <ContactForm handleAddContact={handleAddContact} />
+        <SearchBox searchValue={searchValue} handleSearch={handleSearch} />
+        <ContactList
+          filterContacts={filterContacts}
+          handleDeleteContact={handleDeleteContact}
         />
-      ) : (
-        <Notification />
-      )}
+      </div>
     </>
   );
 }
--- src/components/Notification/Notification.jsx
+++
@@ -1,9 +0,0 @@
-const Notification = () => {
-  return (
-    <div className="wrapper">
-      <p>No feedback yet</p>
-    </div>
-  );
-};
-
-export default Notification;
